8c0312e04e6f5301e16b6c3c77c5b8a936951927
xugr <xugr>
2018-02-23 16:01:27 +0800

    encoder和decoder的demo样例

---
+++ src/main/java/chapter17/three/six/IntegerToStringEncoder.java
@@ -0,0 +1,13 @@
+package chapter17.three.six;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
+import java.util.List;
+
+public class IntegerToStringEncoder extends MessageToMessageEncoder<Integer> {
+    @Override
+    protected void encode(ChannelHandlerContext ctx, Integer msg, List<Object> out) throws Exception {
+        out.add(msg.toString());
+    }
+}
